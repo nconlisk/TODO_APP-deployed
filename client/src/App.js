6014db76e63bebbf33ffcf63b07d88295061f893
nconlisk <noecon@gmail.com>
2023-09-11 14:02:47 +0100

    Updated ListItem.js to enable tasks to be displayed on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 import ListHeader from "./components/ListHeader";
-import { useEffect } from 'react';
-
+import ListItem from "./components/ListItem";
+import { useEffect, useState } from 'react';
 
 const App = () => {
 
-  const getData = async () => {
+  const userEmail = "Ann@test.com" //hard coded until user signup added
+  const [ tasks, setTasks] = useState(null)
 
-    const userEmail = "\ntest@test.com" //hard coded until user signup added
+  const getData = async () => {
     try {
       const response = await fetch(`http://localhost:8000/todos/${userEmail}`)  //url with backticks as will be passing email param.
       const json = await response.json()
+      //console.log(json)
+      setTasks(json)
+
     } catch (err) {
       console.log(err)
     }
@@ -17,9 +21,15 @@ const App = () => {
 
   useEffect(() => getData, [])  // empty [] dependency placed here as we only need to call once.
 
+  console.log(tasks)
+
+  //Sort by date
+  const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
+
   return (
     <div className="app">
       <ListHeader listName={"Holiday tick list"}></ListHeader>
+      { sortedTasks?.map((task) => <ListItem key={task.id} task={task}/>)}
     </div>
   );
 }
--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,6 +1,20 @@
-const ListItem = () => {
+import ProgressBar from "./ProgressBar";
+import TickIcon from "./TickIcon";
+
+const ListItem = ({task}) => {
     return (
-      <div>
+      <div className="list-item">
+        <div className="info-container">
+        <TickIcon/>
+        <p className="task-title">{task.title}</p>
+        <ProgressBar/>
+        </div>
+
+        <div className="button-container">
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+        
       </div>
     );
   }
