f6a5f42be78fb4047a9fa7247e34089ee2399598
nconlisk <noecon@gmail.com>
2023-09-11 12:23:01 +0100

    USERNAME variable renamed to avoid clashing with system variable, to fix db connection issues

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const Pool = require('pg').Pool    // from postgres docs
 require('dotenv').config()         // for using .env for secret info
 
 const pool = new Pool({
-    user: process.env.USERNAME,
+    user: process.env.PGUSERNAME,
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.DBPORT,
--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
 const PORT = process.env.PORT ?? 8000 //this uses a port variable if it exists (e.g. if deployed on a service), otherwise port 8000
 const express = require('express')
 const app = express()
+const pool = require('./db')
+
+//Express test to check routing is working
+app.get('/', (req, res) => {
+    res.send("hello!")
+})
+
+// get all todos
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await pool.query('SELECT * FROM todos')
+        res.json(todos.rows)
+
+    } catch (err) {
+        console.error(err)
+        
+    }
+})
 
 app.listen(PORT, ( )=> console.log(`Server running on PORT ${PORT}`))
