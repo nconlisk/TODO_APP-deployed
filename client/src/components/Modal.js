98d655ae73f9d6af71c271a33066f60f97006a35
nconlisk <noecon@gmail.com>
2023-09-11 18:52:40 +0100

    structure and layout added to modal.js

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,38 @@
 const Modal = () => {
+
+    const mode = 'create'
+    const handleChange = () => {
+        console.log('changing')
+    }
+
     return (
-      <div>
+      <div className="overlay">
+        <div className="modal">
+            <h3>Let's {mode} your task</h3>
+            <button>X</button>
+        </div>
+        <form className="form-title-container">
+            <input
+            required
+            maxLength={30}
+            placeholder=" Your task goes here"
+            name="title"
+            value={""}
+            onChange={handleChange}
+            />
+            <br/>
+            <input 
+            required
+            type="range"
+            min="0"
+            max="100"
+            name="progress"
+            value={""}
+            onChange={handleChange}
+            />
+            <input className={mode} type="submit"/>
+
+        </form>
       </div>
     );
   }
