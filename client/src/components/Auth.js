31a36e8f53cc41feef67f6bc0aa3f7ea7452771b
nconlisk <noecon@gmail.com>
2023-09-13 12:00:30 +0100

    updated login logic in auth.js

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -3,7 +3,12 @@ import { useState } from "react";
 const Auth = () => {
     const [isLogIn, setIsLogin] = useState(true)
     const [error, setError] = useState(null)  
-    const isLogIn = false
+    
+    const viewLogIn = (status) =>{
+      setError(null)
+      setIsLogin(status)
+    }
+
     return (
       <div className="auth-container">
         <div className="auth-container-box">
@@ -16,8 +21,8 @@ const Auth = () => {
             {error && <p>{error}</p>}
           </form>
           <div className="auth-options">
-            <button>Sign Up</button>
-            <butoon>Log In</butoon>
+            <button onClick={() => viewLogIn(false)}>Sign Up</button>
+            <butoon onClick={() => viewLogIn(true)}>Log In</butoon>
           </div>
         </div>
       </div>
