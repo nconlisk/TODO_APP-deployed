c9afd86eadb424423215ff10ec243db53d86a243
nconlisk <noecon@gmail.com>
2023-09-13 11:14:12 +0100

    added basic structure to Auth.js

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,16 +1,24 @@
-const Auth = () => {
-    const isLogIn = false
+import { useState } from "react";
 
+const Auth = () => {
+    const [isLogIn, setIsLogin] = useState(true)
+    const [error, setError] = useState(null)  
+    const isLogIn = false
     return (
       <div className="auth-container">
         <div className="auth-container-box">
           <form>
             <h2>{isLogIn ? 'Please log in' : 'Please sign up!'}</h2>
-            <input type="email"/>
-            <input type="password"/>
-            <input type="password"/>
-            
+            <input type="email" placeholder="email"/>
+            <input type="password" placeholder="password"/>
+            {!isLogIn && <input type="password" placeholder="confirm password"/>}  
+            <input type="submit" className="create"/>
+            {error && <p>{error}</p>}
           </form>
+          <div className="auth-options">
+            <button>Sign Up</button>
+            <butoon>Log In</butoon>
+          </div>
         </div>
       </div>
     );
