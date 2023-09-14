42ddad61a4b8c9e4f5f854e6cfe68c5503c4627e
Noel Conlisk <noecon@gmail.com>
2023-09-14 16:10:03 +0100

    Update db.js
    modified params to match vercel variable names

--- server/db.js
+++ server/db.js
@@ -4,11 +4,11 @@ const Pool = require('pg').Pool    // from postgres docs
 require('dotenv').config()         // for using .env for secret info
 
 const pool = new Pool({
-    user: process.env.PGUSERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    host: process.env.PG_HOST,
     port: process.env.DBPORT,
-    database: 'todoapp'
+    database: process.env.PG_DATABASE
 })
 
 module.exports = pool
