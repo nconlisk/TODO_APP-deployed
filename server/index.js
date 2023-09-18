fa63ebb1563528b818b241e70782af78fa62b50c
nconlisk <noecon@gmail.com>
2023-09-18 16:17:13 +0100

    updated bcrypt to bcryptjs for vercel compatability

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { v4: uuidv4} = require('uuid')  //from documentation used for giving uniq
 const cors = require('cors')
 const app = express()
 const pool = require('./db')
-const bcrypt = require('bcrypt')   //used to hash passwords
+const bcrypt = require('bcryptjs')   //used to hash passwords
 const jwt = require('jsonwebtoken') //used to generate a web authentication token for use in cookie creation
 
 app.use(cors())  //to fix cors error
