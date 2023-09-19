66887dad4fd5de327348e7307ef2d2726afa3492
nconlisk <noecon@gmail.com>
2023-09-19 09:50:27 +0100

    CORS error and db connection updates

--- server/db.js
+++ server/db.js
@@ -1,19 +1,30 @@
 // setting up db for deploying
 
-import { createPool } from '@vercel/postgres';
+const Pool = require('pg-pool');    // from postgres docs
+const url = require('url');
 
+const params = url.parse(process.env.POSTGRES_URL);
+const auth = params.auth.split(':');
 
-//const Pool = require('pg').Pool    // from postgres docs
+const config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+};
 
-const Pool = createPool();
-require('dotenv').config()         // for using .env for secret info
+const pool = new Pool(config);
 
-const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.DBPORT,
-    database: process.env.POSTGRES_DATABASE
-})
+//require('dotenv').config()         // for using .env for secret info
+
+// const pool = new Pool({
+//     user: process.env.POSTGRES_USER,
+//     password: process.env.POSTGRES_PASSWORD,
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.DBPORT,
+//     database: process.env.POSTGRES_DATABASE
+// })
 
 module.exports = pool
