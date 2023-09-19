55b2479c116e56fe158ff36d402ac7d20a673119
nconlisk <noecon@gmail.com>
2023-09-19 09:32:03 +0100

    CORS error

--- server/index.js
+++ server/index.js
@@ -11,28 +11,28 @@ app.use(cors())  //to fix cors error locally
 app.use(express.json())   //to give our app the ability to post json
 
 
-// //Allow cors
-// const allowCors = fn => async (req, res) => {
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     // another common pattern
-//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-//     res.setHeader(
-//       'Access-Control-Allow-Headers',
-//       'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-//     )
-//     if (req.method === 'OPTIONS') {
-//       res.status(200).end()
-//       return
-//     }
-//     return await fn(req, res)
-//   }
+//Allow cors
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
   
-//   const handler = (req, res) => {
-//     const d = new Date()
-//     res.end(d.toString())
-//   }
+  const handler = (req, res) => {
+    const d = new Date()
+    res.end(d.toString())
+  }
 
 //Express test to check routing is working
 app.get('/', (req, res) => {
